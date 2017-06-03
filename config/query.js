d20643f737c46182becf02149caed474e5d5db96
ivan.liu <403704409@qq.com>
2017-06-03 23:53:05 +0800

    把数据库的用户名、密码提取到DATA_BASE_CONFIG.js

--- config/query.js
+++ config/query.js
@@ -1,13 +1,15 @@
 var pg = require('pg')
+
+import {USER, PASSWORD} from '../DATA_BASE_CONFIG.js'
 // create a config to configure both pooling behavior 
 // and client options 
 // note: all config is optional and the environment variables 
 // will be read if the config is not present 
 var config = {
-  user: 'abc', //env var: PGUSER 
+  user: USER, //env var: PGUSER 
   database: 'blog', //env var: PGDATABASE 
-  password: 'abcxcv', //env var: PGPASSWORD 
-  host: '127.0.0.1', // Server hosting the postgres database 
+  password: PASSWORD, //env var: PGPASSWORD 
+  host: '47.88.158.240', // Server hosting the postgres database 
   port: 5432, //env var: PGPORT 
   max: 10, // max number of clients in the pool 
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
