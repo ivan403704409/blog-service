3fc43ae08c607127bd2247afe723aea526934ce0
ivan.liu <403704409@qq.com>
2017-06-14 22:56:18 +0800

    edir

--- controllers/article.js
+++ controllers/article.js
@@ -3,22 +3,26 @@ import * as modelArticle from '../models/article'
 // 添加标签
 export async function add(ctx, next) {
 	let { title, content, tag} = ctx.request.body
-	let id = 2
+	let id = +new Date()
 	let data = {
 		id,
 		title,
 		content,
-		tag: tag || null
+		tag: 'vue'
 	}
 	console.log(data)
 	await modelArticle.add(data)
 	ctx.body = {
 		code: 1,
-		msg: '添加成功'
+		msg: '添加成功',
 	}
 }
 
 export async function list(ctx, next) {
 	let data = await modelArticle.list()
-	ctx.body = data
+	ctx.body = {
+		code: 1,
+		msg: '添加成功',
+		data,
+	}
 }
--- models/article.js
+++ models/article.js
@@ -3,11 +3,12 @@ import query from '../config/query.js'
 
 // 添加文章
 export async function list() {
-	return await query('select * from b_article')
+	return await query('select * from b_article order by ctime desc')
 }
 
 
 // 添加文章
 export async function add({id, title, content, tag=null}) {
+	console.log(id, title, content, tag)
 	await query('insert into b_article (id,title,content,tag) values ($1,$2,$3,$4);', [id, title, content, tag])
 }
