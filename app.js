ad11456ff5a627ea30892df684254a1a533a90a4
ivan.liu <403704409@qq.com>
2017-06-23 15:26:54 +0800

    edit

--- app.js
+++ app.js
@@ -8,35 +8,29 @@ const app = new Koa();
 // body处理
 app.use(bodyParser())
 
+
+// logger
+app.use(async (ctx, next) => {
+    const start = new Date;
+    await next()
+    const ms = new Date - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+});
+
+
 // 跨域处理
 app.use(async (ctx, next) => {
-  await next()
-  // ctx.set('Access-Control-Allow-Origin', 'http://127.0.0.1:9000')
   ctx.set('Access-Control-Allow-Origin', '*')
   ctx.set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
   ctx.set("Access-Control-Allow-Credentials", false)
   ctx.set("Access-Control-Max-Age", '86400')
   ctx.set("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept")
+  await next()
 });
 
 // 路由分配
 import initRoute from './routes'
 initRoute(app)
 
-
-// logger
-app.use((ctx, next) => {
-  const start = new Date;
-  return next().then(() => {
-    const ms = new Date - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-  });
-});
-
-
-// response
-// app.use(ctx => {
-//   ctx.body = 'Hello World';
-// });
 
 app.listen(9099);
