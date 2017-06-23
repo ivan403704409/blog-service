2e88eed112804c23fd0d212a26a62c902e820c72
ivan.liu <403704409@qq.com>
2017-06-23 15:16:06 +0800

    删除注释，修改提示

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,6 @@ ctrlFiles.forEach(file => oCtrls[file.slice(0, -3)] = require( resolve(file) ).d
 
 // 注册路由
 Object.keys(oCtrls).forEach(ctrlName => {
-	// 当前路由有哪些方法['get', 'post', ...]
 	let oCtrl = oCtrls[ctrlName]
 	let aMethodPath = Object.keys(oCtrl)
 	aMethodPath.forEach(methodPath => {
@@ -37,7 +36,7 @@ Object.keys(oCtrls).forEach(ctrlName => {
 		methodPath = methodPath.trim()
 		// 验证是否是合法的格式，'get /path/xxx'
 		if(!/^(get|post|put|delete|options)\s+[^\s]+$/i.test(methodPath)){
-			throw new Error(`路由格式正确： ‘${methodPath}’`)
+			throw new Error(`路由格式不正确： ‘${methodPath}’`)
 		}
 		let [method, sPath] = methodPath.split(/\s+/)
 		let mapPath = path.join(`/${ctrlName}`, sPath)
@@ -45,19 +44,6 @@ Object.keys(oCtrls).forEach(ctrlName => {
 		console.log(mapPath)
 	})
 })
-// Object.keys(oCtrls).forEach(ctrlName => {
-// 	// 当前路由有哪些方法['get', 'post', ...]
-// 	let methods = Object.keys(oCtrls[ctrlName])
-// 	methods.forEach(method => {
-// 		// 当前路由当前方法的所有处理函数
-// 		let ctrlHandlers = oCtrls[ctrlName][method]
-// 		Object.keys(ctrlHandlers).forEach(path => {
-// 			// router[method](path, fn)
-// 			let handler = ctrlHandlers[path]
-// 			router[method.toLowerCase()](`/${ctrlName}/${path}`, handler)
-// 		})
-// 	})
-// })
 
 export default function initRoute(app){
 	app.use(router.routes()).use(router.allowedMethods())
